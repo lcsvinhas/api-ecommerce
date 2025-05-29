b32bd185b9403cbc815b8f77c4add587d136d424
BarackOBaima <baimacaio14@gmail.com>
2025-05-29 16:26:14 -0300

    Criacao de Enumeradores e Excecoes

--- src/main/java/org/serratec/backend/entity/Status.java
+++ src/main/java/org/serratec/backend/entity/Status.java
@@ -1,4 +1,25 @@
 package org.serratec.backend.entity;
 
+import org.serratec.backend.exception.EnumException;
+
 public enum Status {
+
+    FATURADO,
+    CANCELADO,
+    PENDENTE,
+    PROSSESSANDO,
+    ENTREGUE,
+    ENVIADO;
+
+    public static Status verificarStatus(String status) {
+        for (Status s : Status.values()) {
+            if (status.equals(s.name())) {
+                return s;
+            }
+        }
+        throw new EnumException("Status inválido.");
+    }
+
 }
+
+
---
+++ src/main/java/org/serratec/backend/exception/EnumException.java
@@ -0,0 +1,8 @@
+package org.serratec.backend.exception;
+
+public class EnumException extends RuntimeException {
+
+    public EnumException(String message) {
+        super(message);
+    }
+}

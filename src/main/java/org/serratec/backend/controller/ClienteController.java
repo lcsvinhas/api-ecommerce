6540b2ca0bcfed7228506c626cb5c1543c095948
BarackOBaima <baimacaio14@gmail.com>
2025-06-02 15:38:47 -0300

    Individual Caio

--- src/main/java/org/serratec/backend/controller/ClienteController.java
+++ src/main/java/org/serratec/backend/controller/ClienteController.java
@@ -3,9 +3,11 @@ package org.serratec.backend.controller;
 import jakarta.validation.Valid;
 import org.serratec.backend.dto.ClienteRequestDTO;
 import org.serratec.backend.dto.ClienteResponseDTO;
+import org.serratec.backend.dto.PedidoResponseDTO;
 import org.serratec.backend.entity.Cliente;
 import org.serratec.backend.repository.ClienteRepository;
 import org.serratec.backend.service.ClienteService;
+import org.serratec.backend.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,8 @@ import java.util.Optional;
 public class ClienteController {
     @Autowired
     private ClienteService service;
+    @Autowired
+    private PedidoService pedidoService;
 
     @GetMapping
     public ResponseEntity<List<ClienteResponseDTO>> listar() {
@@ -39,6 +43,11 @@ public class ClienteController {
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         service.deletar(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping ("/{id}/pedidos")
+    public ResponseEntity <List<PedidoResponseDTO>> listarPedidosPorCliente(@PathVariable Long id){
+        return ResponseEntity.ok(pedidoService.listarPorCliente(id));
     }
 
 }
--- src/main/java/org/serratec/backend/repository/PedidoRepository.java
+++ src/main/java/org/serratec/backend/repository/PedidoRepository.java
@@ -3,6 +3,10 @@ package org.serratec.backend.repository;
 import org.serratec.backend.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    List<Pedido> findByClienteId(Long id);
 
 }
